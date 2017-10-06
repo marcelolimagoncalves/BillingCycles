307cfb7dac4d65c75f691a0914680ef223df2552
Marcelo de Lima Goncalves <marcelolimagoncalves@gmail.com>
2017-10-05 22:23:58 -0300

    Comitando alterações

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -12,7 +12,7 @@
 
     vm.refresh = function(){
       $http.get(url).then(function(response){
-        vm.billingCycle = {}
+        vm.billingCycle = {credits: [{}], debts: [{}]}
         vm.billingCycles = response.data
         tabs.show(vm, {tabList: true, tabCreate: true})
       })
@@ -37,15 +37,53 @@
       tabs.show(vm, {tabDelete: true})
     }
 
-vm.delete = function(){
-  const deleteUrl = `${url}/${vm.billingCycle._id}`
-  $http.delete(deleteUrl, vm.billingCycle).then(function(response){
-    vm.refresh()
-    msgs.addSuccess('Operação realizada com sucesso!')
-  }).catch(function(response){
-    msgs.addError(response.data.errors)
-  })
-}
+    vm.update = function(){
+      const updateUrl = `${url}/${vm.billingCycle._id}`
+      $http.put(updateUrl, vm.billingCycle).then(function(response){
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response){
+        msgs.addError(response.data.errors)
+      })
+    }
+
+    vm.delete = function(){
+      const deleteUrl = `${url}/${vm.billingCycle._id}`
+      $http.delete(deleteUrl, vm.billingCycle).then(function(response){
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response){
+        msgs.addError(response.data.errors)
+      })
+    }
+
+    vm.addCredit = function(index){
+      vm.billingCycle.credits.splice(index + 1, 0, {})
+    }
+
+    vm.cloneCredit = function(index, {name, value}){
+      vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+    }
+
+    vm.deleteCredit = function(index){
+      if(vm.billingCycle.credits.length > 1){
+        vm.billingCycle.credits.splice(index, 1)
+      }
+    }
+
+    vm.addDebt = function(index){
+      vm.billingCycle.debts.splice(index + 1, 0, {})
+    }
+
+    vm.cloneDebt = function(index, {name, value, status}){
+      vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+    }
+
+    vm.deleteDebt = function(index){
+      if(vm.billingCycle.debts.length > 1){
+        vm.billingCycle.debts.splice(index, 1)
+      }
+    }
 
     vm.refresh()
   }
